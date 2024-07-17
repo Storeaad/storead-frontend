c0477dee65ab601a05af0ef866a09e952e5cf0cf
genTe <128919388+wdgWon@users.noreply.github.com>
2024-07-17 23:54:45 +0900

    [Fix 🪛] noto sans kr 폰트 적용 안되던 문제 해결

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children, loginModal }: RootLayoutProps) {
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.className,
+          fontSans.variable,
         )}
       >
         <ReactQueryProviders>
