c9724887b3551a4cdb5a78659f14a3891b181e35
genTe <128919388+wdgWon@users.noreply.github.com>
2024-07-29 14:23:17 +0900

    [Fix 🪛] 잘못된 endpoint 수정

--- lib/apis/auth/serverVerify.ts
+++ lib/apis/auth/serverVerify.ts
@@ -1,18 +1,22 @@
 "use server";
 
+import { serverInstance } from "../server-instance";
+
 export const serverVerify = async (accessToken: string | undefined) => {
   if (!accessToken) {
     return new Response(null, { status: 401 });
   }
 
-  const response = await fetch(
-    `${process.env.BASE_URL}/api/v1/auth/tokens/verify`,
-    {
-      method: "POST",
-      headers: { Authorization: `Beaer ${accessToken}` },
-      cache: "no-store",
+  const response = await serverInstance({
+    endPoint: `/auth/tokens/verify`,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
-  );
+    body: JSON.stringify({ token: accessToken }),
+    cache: "no-store",
+    includeAuth: false,
+  });
 
   return response;
 };
