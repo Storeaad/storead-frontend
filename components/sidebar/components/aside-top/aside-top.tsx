ffe5453863cadb815961b9bd332d44c7597ba370
genTe <128919388+wdgWon@users.noreply.github.com>
2024-08-14 17:29:44 +0900

    [Chore 🚀] 검색 아이콘 추가 및 검색 타이틀 구성 수정

--- components/sidebar/components/aside-top/aside-top.tsx
+++ components/sidebar/components/aside-top/aside-top.tsx
@@ -1,10 +1,13 @@
 import LogoIcon from "./logo-icon";
+import SearchIcon from "./search-icon";
 
 function AsideTop() {
   return (
     <div className="flex flex-col space-y-4">
       {/* Logo */}
       <LogoIcon />
+      {/* search */}
+      <SearchIcon />
     </div>
   );
 }
