b5f74e8307fd8b0c10a2683d5278cebd6bf327be
wdgWon <same666@naver.com>
2025-02-22 15:14:03 +0900

    배포 오류 수정10

--- lib/apis/auth/googleLogin.ts
+++ lib/apis/auth/googleLogin.ts
@@ -1,10 +1,11 @@
 import { serverInstance } from "../server-instance";
 
 export const googleLogin = (params: URLSearchParams) => {
-  return serverInstance({
-    endPoint: `/auth/connections/google`,
-    method: "GET",
-    params,
-    includeAuth: false,
-  });
+  // return serverInstance({
+  //   endPoint: `/auth/connections/google`,
+  //   method: "GET",
+  //   params,
+  //   includeAuth: false,
+  // });
+  return fetch(`${process.env.BASE_URL}/auth/connections/google?${params}`)
 };
