9f4ad927ccdb57574d89f7625dc58d589ece6964
wdgWon <same666@naver.com>
2025-02-22 15:30:47 +0900

    배포 오류 수정12

--- lib/apis/auth/googleLogin.ts
+++ lib/apis/auth/googleLogin.ts
@@ -1,10 +1,11 @@
 import { serverInstance } from "../server-instance";
 
 export const googleLogin = (params: URLSearchParams) => {
-  return serverInstance({
-    endPoint: `/auth/connections/google`,
-    method: "GET",
-    params,
-    includeAuth: false,
-  });
+  // return serverInstance({
+  //   endPoint: `/auth/connections/google`,
+  //   method: "GET",
+  //   params,
+  //   includeAuth: false,
+  // });
+  return fetch(`https://api.storead.site/api/v1/auth/connections/google?${params}`)
 };
