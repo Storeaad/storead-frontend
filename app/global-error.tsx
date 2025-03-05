a6f2849f5c75da52d10552a0c78ef32cac069787
wdgWon <same666@naver.com>
2025-03-05 20:52:51 +0900

    Sentry 적용

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { LogoutError } from "@/constants/customError";
 import "@/styles/globals.css";
+import * as Sentry from "@sentry/nextjs";
+import Error from "next/error";
+import { useEffect } from "react";
 
 export default function GlobalError({
   error,
@@ -12,13 +15,16 @@ export default function GlobalError({
 }: {
   error: (Error | LogoutError) & { digest?: string };
   reset: () => void;
-}) {
+  }) {
   const router = useRouter();
 
   // if (error instanceof LogoutError) {
   //   console.log("error occuired");
   //   return router.push("/logout");
   // }
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
 
   return (
     <html>
@@ -36,3 +42,4 @@ export default function GlobalError({
     </html>
   );
 }
+ 
---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://559f9c0df542a94bafca936bf256c963@o4508924868558848.ingest.us.sentry.io/4508924884811776",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
