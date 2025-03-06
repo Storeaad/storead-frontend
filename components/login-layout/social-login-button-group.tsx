a778aac31c21f9a00c607e2086a6549472bff0eb
wdgWon <same666@naver.com>
2025-03-06 15:33:59 +0900

    카카오 로컬 로그인 임시 구현

--- components/login-layout/social-login-button-group.tsx
+++ components/login-layout/social-login-button-group.tsx
@@ -15,7 +15,7 @@ function SocialLoginButtonGroup() {
         </Button>
       </a>
       <a
-        href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code&scope=profile_nickname`}
+        href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code&scope=profile_nickname,account_email`}
       >
         <Button
           type="button"
--- lib/apis/auth/kakaoLogin.ts
+++ lib/apis/auth/kakaoLogin.ts
@@ -1,10 +1,18 @@
 import { serverInstance } from "../server-instance";
 
-export const kakaoLogin = (params: URLSearchParams) => {
-  return serverInstance({
-    endPoint: `/auth/connections/kakao`,
-    method: "GET",
-    params,
-    includeAuth: false,
-  });
+//FIXME: serverInstance를 쓰도록 인스턴스 수정 필요
+export const kakaoLogin = async (params: URLSearchParams) => {
+  // return serverInstance({
+  //   endPoint: `/auth/connections/kakao`,
+  //   method: "GET",
+  //   params,
+  //   includeAuth: false,
+  // });
+  const { access_token } = await fetch(`https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=510fbb712daffaec9d6c9c0f7524e008&redirect_uri=http://localhost:3000/auth/kakao&code=${params.get("code")}`).then(data => data.json())
+
+  return fetch(`http://13.124.59.112:8081/api/v1/auth/kakao`, {
+    headers: {
+      'Authorization': `${access_token}`,
+    }
+  })
 };
